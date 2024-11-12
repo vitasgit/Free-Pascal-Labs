d74e9a4f3b972c6db18c5f8626d9a35d71ee1b0b
vitasgit <vibrantfeynman@gmail.com>
2024-11-12 18:28:10 +0500

    update lab 1

--- Lab 1/1.pas
+++ Lab 1/1.pas
@@ -13,13 +13,16 @@ type
 var
     person: TPerson;
     data: array of TPerson;
-    i, j, g, dataLength: integer;
+    i, j, k, g, dataLenght, childrenIDLenght: integer;
     s: string;
 
 begin
     i := 0;
-    dataLength := 1;
-    SetLength(data, dataLength);
+    k := 0;  // индекс массива детей
+    g := 0;  // вывод детей
+
+    dataLenght := 1;
+    setLength(data, dataLenght);
 
     while not eof do
     begin
@@ -27,33 +30,39 @@ begin
         readln(person.gender);
         readln(person.bdate);
         readln(person.id);
-
-        SetLength(person.childrenID, 0);
+		
+		childrenIDLenght := 1;
+		setLength(person.childrenID, childrenIDLenght);
+		
+		k := 0;  // ошибка
         readln(s);
         while s <> '' do
         begin
-            SetLength(person.childrenID, Length(person.childrenID) + 1);  // поменять
-            person.childrenID[High(person.childrenID)] := s;
+			person.childrenID[k] := s;
+			k := k + 1;
+			childrenIDLenght := childrenIDLenght + 1;
+			setLength(person.childrenID, childrenIDLenght);
             readln(s);
         end;
-
-        dataLength := dataLength + 1;
-        SetLength(data, dataLength);
-
+        
+        setLength(person.childrenID, k);
         data[i] := person;
         i := i + 1;
+        dataLenght := dataLenght + 1;
+        setLength(data, dataLenght);
     end;
 
-    for j := 0 to i - 1 do
+    for j := 0 to i-1 do
     begin
-        writeln(j + 1, ')');
+        writeln(j+1, ')');
         writeln('ФИО: ', data[j].fio);
         writeln('Пол: ', data[j].gender);
         writeln('рожд: ', data[j].bdate);
         writeln('Номер: ', data[j].id);
-        writeln('Дети:');
-
-        for g := 0 to High(data[j].childrenID) do
-            writeln(data[j].childrenID[g]);
+        writeln('Дети: ');
+        
+        for g := 0 to high(data[j].childrenID) do writeln (data[j].childrenID[g]);
+        
+        writeln();
     end;
 end.
