f06fa72fece0a27d6e0e95cd1a597163c66e3621
vitasgit <vibrantfeynman@gmail.com>
2024-11-13 18:56:03 +0500

    исправил считывание детей

--- Lab 1/1.pas
+++ Lab 1/1.pas
@@ -30,21 +30,22 @@ begin
         readln(person.bdate);
         readln(person.id);
 		
-		childrenIDLenght := 1;
+		childrenIDLenght := 0;
 		setLength(person.childrenID, childrenIDLenght);
 		
 		k := 0;  // индекс массива детей
         readln(s);
         while s <> '' do
         begin
+            childrenIDLenght := childrenIDLenght + 1;
+			setLength(person.childrenID, childrenIDLenght);
+
 			person.childrenID[k] := s;
 			k := k + 1;
-			childrenIDLenght := childrenIDLenght + 1;
-			setLength(person.childrenID, childrenIDLenght);
             readln(s);
         end;
         
-        setLength(person.childrenID, k);
+        //setLength(person.childrenID, k);
         data[i] := person;
         i := i + 1;
         dataLenght := dataLenght + 1;
@@ -79,9 +80,7 @@ begin
     end;
 
     // Дана дата, найти всех женщин, родившихся в этот день
-    //day := '06.12';
-    write('Введите дату для поиска женщин: ');
-    readln(day);  // не работает readln()
+    day := '06.12';
 
     for j := 0 to i-1 do
     begin
