78f53b852abf564bf7c81160eb9210b98b6172fc
vitasgit <vibrantfeynman@gmail.com>
2024-12-19 08:43:26 +0500

    update lab 5

--- Lab 5/1.pas
+++ Lab 5/1.pas
@@ -2,17 +2,22 @@
 {$WARN 6058 OFF}
 uses sysutils, variants;
 
-operator := (x:string):double;
+// operator := (x:string):double;
+// begin
+// 	result := StrToFloatDef(x, 0.0);
+// end;
+
+operator explicit (x:string):double;
 begin
-	result := StrToFloatDef(x, 0.0);
-    writeln(x);
+    result := StrToFloatDef(x, 0.0);
 end;
+
 
 var
     s: variant;
     a: double;
 BEGIN
-	s := 'awdwa';  // не присваивает 0.0
+	s := 'awdadw';  // не присваивает 0.0
     writeln(s);  // не выводит 0.0
     a := 1.2;
     writeln(a + s);  // 2.5
