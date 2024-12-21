029b8fae01db990cb2412853ac1334d56177725d
vitasgit <vibrantfeynman@gmail.com>
2024-12-21 19:25:13 +0500

    add units getMax

--- Lab 4/funcs.pas
+++ Lab 4/funcs.pas
@@ -1,8 +1,18 @@
 {$mode objfpc}
+unit funcs;
+interface
+
+function getMax(a, b:integer):integer;
+function getMax(a, b, c:integer):integer;
+function getMax(a, b, c, d:integer):integer;
+function getMax(a, b, c, d, e:integer):integer;
+
+
+implementation
 
 function getMax(a, b:integer):integer;
 begin
-    if (a >= b) then 
+    if (a > b) then 
         result := a
     else 
         result := b;
@@ -22,9 +32,6 @@ function getMax(a, b, c, d, e:integer):integer;
 begin
     result := getMax(getMax(a, b, c, d), e); 
 end;
+// end getMax
 
-
-BEGIN
-    writeln(getMax(5, 4, 3, 3, 0));
-END.
-
+end.
---
+++ Lab 4/main.pas
@@ -0,0 +1,8 @@
+{$mode objfpc}
+uses funcs;
+
+
+BEGIN
+    writeln(getMax(5, 4, 3, 3, 0));
+END.
+
