0ab3480b4c266a9286ffaf439c444f7ae92e56d2
vitasgit <vibrantfeynman@gmail.com>
2024-12-22 17:09:29 +0500

    add units getIntFrac

--- Lab 4/funcs.pas
+++ Lab 4/funcs.pas
@@ -7,6 +7,13 @@ function getMax(a, b, c:integer):integer;
 function getMax(a, b, c, d:integer):integer;
 function getMax(a, b, c, d, e:integer):integer;
 
+function getType(x:integer):string;
+function getType(x:real):string;
+function getType(x:string):string;
+function getType(x:boolean):string;
+
+procedure getIntFrac(x:real; var intPart, fracPart:real);
+
 
 implementation
 
@@ -14,7 +21,7 @@ function getMax(a, b:integer):integer;
 begin
     if (a > b) then 
         result := a
-    else 
+    else
         result := b;
 end;
 
@@ -33,5 +40,33 @@ begin
     result := getMax(getMax(a, b, c, d), e); 
 end;
 // end getMax
+
+function getType(x:integer):string;
+begin
+    result := 'integer';
+end;
+
+function getType(x:real):string;
+begin
+    result := 'real';
+end;
+
+function getType(x:string):string;
+begin
+    result := 'string';
+end;
+
+function getType(x:boolean):string;
+begin
+    result := 'boolean';
+end;
+// end getType
+
+// где ошибка
+procedure getIntFrac(x:real; var intPart, fracPart:real);
+begin
+    intPart := int(x);
+    fracPart := frac(x);
+end;
 
 end.
--- Lab 4/main.pas
+++ Lab 4/main.pas
@@ -1,8 +1,13 @@
 {$mode objfpc}
 uses funcs;
-
+var
+    x: real = 123.026;
+    int_x, frac_x: real;
 
 BEGIN
-    writeln(getMax(5, 4, 3, 3, 0));
+    //writeln(getMax(5, 4, 3, 3, 0));
+    //writeln(getType(a));
+    getIntFrac(x, int_x, frac_x);
+    writeln(x:0:4, ' ',  int_x:0:1, ' ',  frac_x:0:4);
 END.
 
