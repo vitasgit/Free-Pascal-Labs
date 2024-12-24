63fc53fe9788dacd0ccad255504bfe469233bb44
vitasgit <vibrantfeynman@gmail.com>
2024-12-24 19:26:14 +0500

    case for validatedAll

--- Lab 4/main.pas
+++ Lab 4/main.pas
@@ -7,7 +7,7 @@ var
     frac_x: real;
     s: string = 'abc123 *7., h ';
     countDigits, countSpaces, countOther: integer;
-    i, j, k: integer;
+    i, j, k, x1, x2, x3, x4, x5: integer;
 
 BEGIN
     // writeln(getMax(5, 4, 3, 3, 0));
@@ -23,9 +23,36 @@ BEGIN
     // writeln(test_getStrChr());
 
     // writeln(validatedAll());
-    
 
-     
+    if validatedAll then
+    begin
+        writeln('1: getMax');
+        writeln('2: getType');
+        writeln();
+
+        writeln('Введите номер функции(0 - выход): ');
+        readln(i);
+
+        while i <> 0 do
+        begin
+            case i of 
+                1: 
+                begin
+                    write('Введите 5 чисел: ');
+                    readln(x1, x2, x3, x4, x5);
+                    writeln('Макс(', x1, ', ', x2,'): ', getMax(x1, x2));
+                    writeln('Макс(', x1, ', ', x2, ', ', x3, '): ', getMax(x1, x2, x3));
+                    writeln('Макс(', x1, ', ', x2, ', ', x3, ', ', x4, '): ', getMax(x1, x2, x3, x4));
+                    writeln('Макс(', x1, ', ', x2, ', ', x3, ', ', x4, ', ', x5, '): ', getMax(x1, x2, x3, x4, x5));
+                end;
+            end;
+            
+            writeln();
+            writeln('Введите номер функции(0 - выход): ');
+            readln(i);
+        end;
+
+    end;
 
 
 END.
